56e12516f84d9ad164a037e4267f4fd2e92c22ca
zahwanugrah83 <41908073+zahwanugrah83@users.noreply.github.com>
2018-10-29 16:28:13 +0800

    Update debian8.sh

--- debian8.sh
+++ debian8.sh
@@ -18,42 +18,6 @@ apt-get -y install pritunl mongodb-org
 systemctl start mongod pritunl
 systemctl enable mongod pritunl
 
-# Install Squid
-apt-get -y install squid3
-cat > /etc/squid3/squid.conf <<-END
-acl localhost src 127.0.0.1/32 ::1
-acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
-acl SSL_ports port 443
-acl Safe_ports port 80
-acl Safe_ports port 21
-acl Safe_ports port 443
-acl Safe_ports port 70
-acl Safe_ports port 210
-acl Safe_ports port 1025-65535
-acl Safe_ports port 280
-acl Safe_ports port 488
-acl Safe_ports port 591
-acl Safe_ports port 777
-acl CONNECT method CONNECT
-acl SSH dst xxxxxxxxx-xxxxxxxxx/32
-http_access allow SSH
-http_access allow manager localhost
-http_access deny manager
-http_access allow localhost
-http_access deny all
-http_port 8080
-http_port 8000
-http_port 3128
-coredump_dir /var/spool/squid3
-refresh_pattern ^ftp: 1440 20% 10080
-refresh_pattern ^gopher: 1440 0% 1440
-refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
-refresh_pattern . 0 20% 4320
-visible_hostname daybreakersx
-END
-sed -i $MYIP2 /etc/squid3/squid.conf;
-service squid3 restart
-
 # Enable Firewall
 sudo ufw allow 22,80,81,222,443,8080,9700,60000/tcp
 sudo ufw allow 22,80,81,222,443,8080,9700,60000/udp
@@ -75,6 +39,43 @@ wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/zero9911/p
 sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
 service php5-fpm restart
 service nginx restart
+
+#squid proxy
+apt-get -y install squid3
+cat > /etc/squid3/squid.conf <<-END
+acl manager proto cache_object
+acl localhost src 127.0.0.1/32 ::1
+acl to_localhost dst 127.0.0.0/8 0.0.0.0/32 ::1
+acl SSL_ports port 443
+acl Safe_ports port 80
+acl Safe_ports port 21
+acl Safe_ports port 443
+acl Safe_ports port 70
+acl Safe_ports port 210
+acl Safe_ports port 1025-65535
+acl Safe_ports port 280
+acl Safe_ports port 488
+acl Safe_ports port 591
+acl Safe_ports port 777
+acl CONNECT method CONNECT
+acl SSH dst xxxxxxxxx-xxxxxxxxx/255.255.255.255
+http_access allow SSH
+http_access allow manager localhost
+http_access deny manager
+http_access allow localhost
+http_access deny all
+http_port 8000
+http_port 8080
+http_port 3128
+coredump_dir /var/spool/squid3
+refresh_pattern ^ftp: 1440 20% 10080
+refresh_pattern ^gopher: 1440 0% 1440
+refresh_pattern -i (/cgi-bin/|\?) 0 0% 0
+refresh_pattern . 0 20% 4320
+visible_hostname Fedora-Computer
+END
+sed -i $MYIP2 /etc/squid3/squid.conf;
+service squid3 restart
 
 # Install Vnstat
 apt-get -y install vnstat
